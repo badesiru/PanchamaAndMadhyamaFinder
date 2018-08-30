18821ea074d3be613d0443ff3b4f94d553139b65
Siri Rao <badesiru@gmail.com>
2018-08-29 21:10:55 -0400

    first commit

---
+++ panchama.rb
@@ -0,0 +1,19 @@
+puts "Please enter the shruthi of which you want to find the panchama and madhyama of "
+
+shruthi = gets.chomp
+
+if shruthi == "A"
+  puts "The panchama of A is D and the madhyama of A is E"
+elsif shruthi == "B"
+  puts "The panchama of B is E and the madhayama of B is F"
+elsif shruthi == "C"
+  puts "The panchama of C is F and the madhyama of C is G"
+elsif shruthi == "D"
+  puts "The panchama of D is G and the madhyama of A"
+elsif shruthi == "E"
+  puts "The panchama of E is A and the madhyama of E is B"
+elsif shruthi == "F"
+  puts "The panchama of F is B and the madhyama of F is C"
+elsif shruthi == "G"
+  puts "The panchama of G is C and the madhyama of G is D"
+end
